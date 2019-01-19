614477daa07e12b68b432152a96472b6028d4c44
Jose Geraldo Brito <jgabrito@gmail.com>
2019-01-19 20:30:27 -0200

    Finer grained load progress

--- client/main.js
+++ client/main.js
@@ -1,41 +1,100 @@
 import { Meteor } from 'meteor/meteor'
 import { get_default_specialty_modality_pair } from '../imports/api/base_metadata.js'
 
-let MyVue
+let Vue
+let MainWindow
 let main_window
 
 Meteor.startup(() => {
   const el = document.getElementById('root')
 
-  import('../imports/startup/client/startup.js').then(({ Vue, load_main_window }) => {
-    // TODO: here Vue could be used to render some fancier intermediate screen
-    MyVue = Vue
-    el.innerHTML = '<div><p> Carregando... 50% </p></div>'
-    return load_main_window()
-  })
-    .then(({ MainWindow }) => {
-      const modspec = get_default_specialty_modality_pair(true)
-      // Show the main App window in its full splendour
-      main_window = new MyVue({
-        el,
-        template : `
-        <MainWindow 
-          v-bind:initial-modality="modality"
-          v-bind:initial-specialty="specialty"
-          v-bind:local-vue="local_vue"
-        >
-        </MainWindow>
-      `,
-        data : {
-          modality : modspec.modality,
-          specialty : modspec.specialty,
-          local_vue : MyVue
+  const load_promises = []
+  let resolved_count = 0
+
+  const document_resource_checker_factory = (name) => {
+    return new Promise((resolve, reject) => {
+      const me = setInterval(
+        () => {
+          const resource = document[name]
+          if (resource !== undefined) {
+            if (resource.error) {
+              reject()
+            }
+            else {
+              resolve()
+            }
+            clearInterval(me)
+          }
         },
-        components: { MainWindow }
+        500
+      )
+    })
+    .then(() => {
+      resolved_count += 1
+    })
+  } 
+
+  load_promises.push(document_resource_checker_factory('roboto_css'))
+  load_promises.push(document_resource_checker_factory('material_icons_css'))
+  load_promises.push(
+    import('../imports/startup/client/startup.js').then((content) => {
+      Vue = content.Vue
+      const { load_main_window } = content
+  
+      // TODO: here Vue could be used to render some fancier intermediate screen
+      load_promises.push(load_main_window().then((content) => {
+        MainWindow = content.MainWindow
+        resolved_count += 1     
+      }))
+
+      resolved_count += 1
+    })
+  )
+
+  const promise_checker_factory = () => {
+    return Promise.all(Array.from(load_promises))
+      .then(() => {
+        if (resolved_count < load_promises.length) {
+          return promise_checker_factory()
+        }
       })
+  }
+  
+  promise_checker_factory().then(() => {
+    // Show the main App window in its full splendour
+    const modspec = get_default_specialty_modality_pair(true)
+    main_window = new Vue({
+      el,
+      template : `
+      <MainWindow 
+        v-bind:initial-modality="modality"
+        v-bind:initial-specialty="specialty"
+        v-bind:local-vue="local_vue"
+      >
+      </MainWindow>
+    `,
+      data : {
+        modality : modspec.modality,
+        specialty : modspec.specialty,
+        local_vue : Vue
+      },
+      components: { MainWindow }
     })
-    .catch((err) => {
-      el.innerHTML = '<p> Erro carregando componentes básicos. Recarregue por favor. </p>'
-      console.log(err)
-    })
+  })
+  .catch (() => {
+    el.innerHTML = '<p> Erro carregando componentes básicos. Recarregue por favor. </p>'
+    console.log(err)
+  })
+  
+  const me = setInterval(
+    () => {
+      if (resolved_count >= load_promises.length) {
+        clearInterval(me)
+        return
+      }
+      const progress = 100.0  * resolved_count / load_promises.length
+      el.innerHTML = `<div><p>Carregando... ${progress.toFixed(0)}% </p></div>`
+    },
+    200
+  )
 })
