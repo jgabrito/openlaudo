fe16a010490627270a018fc67e73ba51d08ebee1
Jose Geraldo Brito <jgabrito@gmail.com>
2019-01-20 13:16:02 -0200

    Metadata mixin documentation and robustness improvements

--- imports/ui/components/mixins/metadata_mixin.js
+++ imports/ui/components/mixins/metadata_mixin.js
@@ -1,13 +1,25 @@
+import _values from 'lodash/values'
 import _sortBy from 'lodash/sortBy'
 import _property from 'lodash/property'
 import _filter from 'lodash/filter'
+import _isString from 'lodash/isString'
 
 import base_metadata from '../../../api/base_metadata.js'
 
 function valid_modality_for_specialty (modality, specialty) {
+  if (! specialty) return false
+  if (! modality) return false
   if (specialty.modalities === 'all') return true
   return specialty.modalities.includes(modality.name)
 }
+
+/*
+  This mixin adds metadata navigation functionality to a component. 
+  It adds a current_modality and current_specialty to the data object and methods
+  to manipulate these, making sure the pair is always valid. It also computes 
+  emits changed events for these data items and computes modality and specialty lists
+  including in the latter only the valid specialties for the current modality.
+*/
 
 export default {
   data: function () {
@@ -25,11 +37,21 @@ export default {
 
   computed: {
     modalities: function () {
-      return _sortBy(Object.values(this.metadata.modalities), _property('nickname'))
+      return _sortBy(_values(this.metadata.modalities), _property('nickname'))
     },
 
     specialties: function () {
       return this.get_valid_specialties()
+    }
+  },
+
+  watch : {
+    current_modality : function() {
+      this.$emit('modality-changed', this.current_modality)
+    },
+
+    current_specialty : function() {
+      this.$emit('specialty-changed', this.current_specialty)
     }
   },
 
@@ -42,17 +64,23 @@ export default {
       return specialties
     },
 
-    set_modality: function (new_modality_name) {
+    set_modality: function (new_modality) {
+      let new_modality_name = new_modality
+      if (! _isString(new_modality_name)) {
+        new_modality_name = new_modality_name.name
+      }
       this.current_modality = this.metadata.modalities[new_modality_name]
-      this.$emit('modality-changed', this.current_modality)
       if (!valid_modality_for_specialty(this.current_modality, this.current_specialty)) {
         this.set_specialty(this.get_valid_specialties()[0].name)
       }
     },
 
-    set_specialty: function (new_specialty_name) {
+    set_specialty: function (new_specialty) {
+      let new_specialty_name = new_specialty
+      if (! _isString(new_specialty_name)) {
+        new_specialty_name = new_specialty_name.name
+      }
       this.current_specialty = this.metadata.specialties[new_specialty_name]
-      this.$emit('specialty-changed', this.current_specialty)
     }
   }
 }
